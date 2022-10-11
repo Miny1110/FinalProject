65c1a24599d981512f0ed8b61b5ed0da5c4894f7
Miny1110 <wjdalswjd453@naver.com>
2022-10-11 16:19:32 +0900

    프로젝트 세팅

---
+++ src/main/java/com/exe/cozy/CozyHouseApplication.java
@@ -0,0 +1,13 @@
+package com.exe.cozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/cozy/CozyHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.cozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozyHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

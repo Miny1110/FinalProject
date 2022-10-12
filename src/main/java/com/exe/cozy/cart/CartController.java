e02e58599c5958bb11fd3929f7a87096c7e525bc
eunji_jeon <jej940@gmail.com>
2022-10-12 11:41:36 +0900

    상세페이지

--- src/main/java/com/exe/cozy/cart/CartController.java
+++ src/main/java/com/exe/cozy/cart/CartController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CartController {
 
+    @RequestMapping("product-bottom-thumbnail.html")
+    public String detail(){ return "product-bottom-thumbnail"; }
     @RequestMapping("/cart.*")
     public String cart(){ return "cart";}
 

47d4ab1de41db7ac1a9272e9e7a8c0f8a4da8272
eunji_jeon <jej940@gmail.com>
2022-10-13 12:11:10 +0900

    itemMapper dto 수정 //db연결수정

--- src/main/java/com/exe/cozy/itemDetail/ItemDetailServiceImpl.java
+++ src/main/java/com/exe/cozy/itemDetail/ItemDetailServiceImpl.java
@@ -1,14 +1,13 @@
 package com.exe.cozy.itemDetail;
 
-import com.exe.cozy.mapper.ItemDetailMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("itemDetailService")
 public class ItemDetailServiceImpl implements ItemDetailService {
 	
-	@Autowired
-	private ItemDetailMapper itemDetailMapper;
+
+
 
 
 }

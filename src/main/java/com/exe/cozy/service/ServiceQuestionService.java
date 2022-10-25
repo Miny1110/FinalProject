47b2103227469acd55ab23d15ced3f33a9d2cbd9
ForMeForEJ <106376139+ForMeForEJ@users.noreply.github.com>
2022-10-25 20:58:42 +0900

    qna

---
+++ src/main/java/com/exe/cozy/service/ServiceQuestionService.java
@@ -0,0 +1,36 @@
+package com.exe.cozy.service;
+
+import java.util.List;
+
+import com.exe.cozy.domain.ReplyDto;
+import com.exe.cozy.domain.ServiceQuestionDto;
+import com.github.pagehelper.Page;
+
+
+
+public interface ServiceQuestionService {
+
+	public int serviceQueMaxNum() throws Exception;
+
+	/* 문의등록 */
+	public void insertServiceQue(ServiceQuestionDto sqdto) throws Exception;
+	
+	/* 문의게시글번호로 불러오기 */
+	public ServiceQuestionDto findServiceQue(int serviceQueNum) throws Exception;
+
+	/* 문의목록불러오기 */
+	public List<ServiceQuestionDto> getReadServiceQueData(int serviceQueNum) throws Exception;
+
+	public List<ServiceQuestionDto> getReadServiceQueList(int serviceQueNum) throws Exception;
+	
+	/* 페이징 */
+	public Page<ServiceQuestionDto> getServiceQuePaging(int pageNum);
+	
+	/* 문의수정 */
+	public void updateServiceQue(ServiceQuestionDto sqdto) throws Exception;
+	
+	/*문의삭제*/
+	public void deleteServiceQue(int serviceQueNum) throws Exception;
+
+	
+}

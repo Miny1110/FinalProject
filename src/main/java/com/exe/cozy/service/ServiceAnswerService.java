bf2900f62501b125db6b2bbf6da1468f6ddd46ec
xxnzi <lej0554@naver.com>
2022-10-28 14:32:10 +0900

    서비스 매퍼

--- src/main/java/com/exe/cozy/service/ServiceAnswerService.java
+++ src/main/java/com/exe/cozy/service/ServiceAnswerService.java
@@ -4,7 +4,22 @@ import com.exe.cozy.domain.ServiceAnswerDto;
 
 public interface ServiceAnswerService {
 
+	public int serviceAnsMaxNum() throws Exception;
+	
+	/* 답변 등록 */
+	public void insertServiceAns(ServiceAnswerDto sadto) throws Exception;
+	
+	/* 답변 게시글 번호로 불러오기*/
 	public ServiceAnswerDto findServiceAns(int serviceAnsNum) throws Exception;
+	
+	/* 답변 불러오기*/
 	public ServiceAnswerDto getReadServiceAnsData(int serviceAnsNum) throws Exception;
+	
+	/* 답변 수정*/
+	public void updateServiceAns(ServiceAnswerDto sadto) throws Exception;
+	
+	/* 문의 삭제*/
+	public void deleteServiceAns(int serviceAnsNum) throws Exception;
+	
 	
 }

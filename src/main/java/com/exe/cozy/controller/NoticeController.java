2f58b1b80f6f8a690173682d225882f971ecae25
xxnzi <lej0554@naver.com>
2022-11-07 01:42:51 +0900

    수정
    자잘한 에러 수정

--- src/main/java/com/exe/cozy/controller/NoticeController.java
+++ src/main/java/com/exe/cozy/controller/NoticeController.java
@@ -1,19 +1,13 @@
 package com.exe.cozy.controller;
 
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.List;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.exe.cozy.domain.NoticeDto;
